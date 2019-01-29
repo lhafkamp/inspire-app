9fd6cd8cef79c5ba5801ca08ff205ffce0f59d88
Luuk <luukhafkamp@gmail.com>
2019-01-29 11:24:57 +0100

    Set up wireframe grid

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 import Nav from './Nav'
+import Footer from './Footer'
+import CourseSection from './CourseSection'
+import FilterSection from './FilterSection'
+
 import '../styles/all.scss'
 
 const Layout = () => (
   <React.Fragment>
     <Nav />
     <div id="layout">
-
+      <div>
+        <FilterSection />
+        <CourseSection />
+      </div>
+      <Footer />
     </div>
   </React.Fragment>
 )
