9ad37217b53bc258afb716488846b15c1e264b91
Luuk <luukhafkamp@gmail.com>
2019-01-29 11:58:13 +0100

    Display courses

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Course = () => (
-  <div className="course">
-    
-  </div>
-)
+const Course = ({ courseData }) => {
+  const { name, description, category, price } = courseData
+
+  return (
+    <div className="course">
+      <p>{name}</p>
+      <p>{description}</p>
+      <p>{category}</p>
+      <p>{price}</p>
+    </div>
+  )
+}
 
 export default Course
--- src/components/CourseSection.js
+++ src/components/CourseSection.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import allCourses from '../allCourses'
 
-const CourseSection = () => (
-  <div id="course-section">
-    
-  </div>
-)
+import Course from './Course'
+
+export class CourseSection extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      courses: []
+    }
+  }
+
+  componentDidMount() {
+    const arr = []
+    Object.keys(allCourses).map(key => arr.push(allCourses[key]))
+
+    this.setState({
+      courses: arr
+    })
+  }
+
+  render() {
+    const { courses } = this.state
+
+    return (
+      <div id="course-section">
+        {courses.map(course => <Course courseData={course} key={course.name} />)}
+      </div>
+    )
+  }
+}
+
 
 export default CourseSection
