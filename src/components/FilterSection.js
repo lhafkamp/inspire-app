6ad76784988a9b293e96dff6dd4dda0dd185a49f
Luuk <luukhafkamp@gmail.com>
2019-01-30 12:05:53 +0100

    Styling the filter-section

--- src/components/FilterSection.js
+++ src/components/FilterSection.js
@@ -4,20 +4,33 @@ import 'nouislider/distribute/nouislider.css'
 
 const FilterSection = ({ onChange }) => (
   <div id="filter-section">
-    <input type="text" onChange={onChange} />
-    <select onChange={onChange}>
-      <option value="all">all categories</option>
-      <option value="calculus">calculus</option>
-      <option value="language">language</option>
-      <option value="history">history</option>
-    </select>
-    <Slider 
-      range={{ min: 0, max: 300 }} 
-      start={[0, 300]}
-      tooltips={true}
-      connect
-      onChange={onChange} 
-    />
+    <h3>Find your course</h3>
+
+    <div className="search">
+      <label>Search</label>
+      <input type="text" onChange={onChange} placeholder="Search for courses.." />
+    </div>
+
+    <div className="categories">
+      <label>Categories</label>
+      <select onChange={onChange}>
+        <option value="all">all categories</option>
+        <option value="calculus">calculus</option>
+        <option value="language">language</option>
+        <option value="history">history</option>
+      </select>
+    </div>
+
+    <div className="price-range">
+      <label>Price range</label>
+      <Slider 
+        range={{ min: 0, max: 300 }} 
+        start={[0, 300]}
+        tooltips={true}
+        connect
+        onChange={onChange} 
+      />
+    </div>
   </div>
 )
 
